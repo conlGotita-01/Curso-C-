bfd9e2f35577c725c056c71e7956730478b83df2
Brenda Torres Murcia <brenda13071@hotmail.com>
2023-10-03 16:51:15 -0500

    Lectura|Entrada De Datos En C++

---
+++ entradaDeDatos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main (){
+
+    int numero = 0;
+
+    cout << "Ingrese un numero entero: " << endl;
+    cin >> numero;
+    cout << "El numero digitado es: " << numero;
+    
+
+    return 0;
+}
