78c933b4ebbf9293ccc7d7b3f7138095b4b82670
Brenda Torres Murcia <brenda13071@hotmail.com>
2023-10-03 18:23:31 -0500

    Expresion Matematica 3.0 En C++

---
+++ expresionMat3.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main ( ){
+    
+    float a, b, c, d, e, f, resultado = 0 ;
+    
+    cout << " Ingrese el valor para a: " ;
+    cin >> a ;
+    
+    cout << " Ingrese el valor para b: " ;
+    cin >> b ;
+    
+    cout << " Ingrese el valor para c: " ;
+    cin >> c ;
+    
+    cout << " Ingrese el valor para d: " ;
+    cin >> d ;
+
+    cout << " Ingrese el valor para d: " ;
+    cin >> e ;
+
+    cout << " Ingrese el valor para d: " ;
+    cin >> f ;
+
+
+    resultado = ( a + ( b / c ) ) / ( d + ( e / f ) ) ;
+
+    cout.precision(2) ; // Para redondear y que salgan dos decimales después de la coma. 
+    
+    cout << " El resultado es: " << resultado ;
+    
+    return 0 ;
+}
