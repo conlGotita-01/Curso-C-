1b8c8d995aeb2e74c3133cb354e74272b59c828b
Brenda Torres Murcia <brenda13071@hotmail.com>
2023-10-03 17:05:01 -0500

    Tipos De Datos En C++

--- tiposDeDatos.cpp
+++ tiposDeDatos.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(){
 
-    int entero = 10;
-    float flotante = 15.23;
-    double doble = 13.435334;
-    char letra = ' A ';
+    int entero = 10 ;
+    float flotante = 15.23 ;
+    double doble = 13.435334 ;
+    char letra = ' A ' ;
 
 
 
