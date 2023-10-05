d7361279b2d2864d1656dce75c39dfd24fc409b6
Brenda Torres Murcia <brenda13071@hotmail.com>
2023-10-05 18:14:48 -0500

    Promedio De Notas En C++

--- notasPromedio.cpp
+++ notasPromedio.cpp
@@ -8,26 +8,26 @@ using namespace std;
 
 
 
-int main ( ){
+int main(){
     
     
-    float nota1 , nota2 , nota3 , resultado ;
+    float nota1, nota2, nota3, resultado;
     
-    cout << " Ingrese la nota numero 1: " ;
-    cin >> nota1 ;
+    cout << " Ingrese la nota numero 1: ";
+    cin >> nota1;
     
-    cout << " Ingrese la nota numero 2: " ;
-    cin >> nota2 ;
+    cout << " Ingrese la nota numero 2: ";
+    cin >> nota2;
     
-    cout << " Ingrese la nota numero 3: " ;
-    cin >> nota3 ;
-    
-    
-    resultado = ( nota1 + nota2 + nota3 ) / 3 ;
-    cout.precision(2) ;
-    
-    cout << " El promedio de las notas es: " << resultado << endl ;
+    cout << " Ingrese la nota numero 3: ";
+    cin >> nota3;
     
     
-    return 0 ;
+    resultado = ( nota1 + nota2 + nota3 ) / 3;
+    cout.precision(2);
+    
+    cout << " El promedio de las notas es: " << resultado << endl;
+    
+    
+    return 0;
 }
